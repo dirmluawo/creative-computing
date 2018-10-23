b4fde6729a5de147feefb633c5cdb32844ea4a42
Dirm <luawd369@newschool.edu>
2018-10-23 09:34:11 -0400

    commit

--- 06/app.js
+++ 06/app.js
@@ -1,9 +1,9 @@
 const messageWrapper = document.getElementById('message');
 
-const a = prompt ('Input a number to mutliply');
-const b = prompt ('Input another number to mutliply');
-const c = prompt ('Input a number to add');
-const d = prompt ('Input a number to subtract');
+const a = parseInt(prompt ('Input a number to mutliply'));
+const b = parseInt(prompt ('Input another number to mutliply'));
+const c = parseInt(prompt ('Input a number to add'));
+const d = parseInt(prompt ('Input a number to subtract'));
 
 const x = (a * b) + c - d;
 
