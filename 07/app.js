2650df8703dcea7a625361ae8da54cf78e1bc9ee
Dirm <luawd369@newschool.edu>
2018-11-06 07:09:10 -0500

    commit

--- 07/app.js
+++ 07/app.js
@@ -1,6 +1,8 @@
 const messageWrapper = document.getElementById('message');
 
 const a = parseInt(prompt ('Input a number to mutliply'));
+if (a == null || a == "") {
+    a = 0;
 const b = parseInt(prompt ('Input another number to mutliply'));
 const c = parseInt(prompt ('Input a number to add'));
 const d = parseInt(prompt ('Input a number to subtract'));
--- 08/keycoode.js
+++ 08/keycoode.js
@@ -1,6 +1,104 @@
-const messageWrapper = document.getElementById('message');
+const originalHTML = document.body.innerHTML;
+let counter = 0;
+let messageWrapper = document.getElementById('message');
 
-var x = event.keyCode;
+document.onkeydown = function (event) {
+
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.body.innerHTML = originalHTML;
+        messageWrapper = document.getElementById('message');
+      }
+}
+
+document.onkeyup = function (event) {
+    
+    var keyCodePressed = event.keyCode;
+    var fontFamilyArray = ["Anton" , "Lobster", "Monoton", "Abril+Fatface", "Alfa+Slab+One", "Archivo+Black", "Baloo+Bhaijaan", "Bevan", "Fascinate+Inline", "Notable", "Righteous", "Ultra"];
+    const coordinates = helpers.generateCoordinates();
+    const degreesRotation = Math.floor(Math.random() * 360);
+    ft = Math.floor(Math.random() * 12); 
+
+    const dog = document.createElement('h1');
+    dog.className = 'dog';
+    dog.innerText = event.key;
+    dog.style.position = 'absolute';
+    dog.style.fontFamily = fontFamilyArray[ft];
+    dog.style.left = `${coordinates.x}px`;
+    dog.style.top = `${coordinates.y}px`;
+    dog.style.transform = `-webkit-rotate(${degreesRotation}deg)`;
+    dog.style.transform = `rotate(${degreesRotation}deg)`;
+    document.body.appendChild(dog);
+
+    messageWrapper.innerText = event.key;
+
+    counter++;
+    // always keeps the message on top of other elements
+    messageWrapper.style.zIndex = counter;
+}
 
 
-messageWrapper.innerText = message;
+//const phrase = document.createElement('h3');
+//phrase.className = 'phrase';
+//phrase.style.left = `${coordinates.x}px`;
+//phrase.style.top = `${coordinates.y}px`;
+//phrase.innerText = getPhrase();
+//phrase.style.color = helpers.generateRandomColorString(false);
+//phrase.style.transform = `rotate(${degreesRotation}deg)`;
+//document.body.appendChild(phrase);
+
+
+    //<p id="demo" onclick="myFunction()">Click me to change my text color.</p>
+
+    //<p>A function is triggered when the p element is clicked. The function sets the color of the p element to red.</p>
+
+    //<script>
+    //function myFunction() {
+    //   document.getElementById("demo").style.color = "red";
+    //}
+
+document.onclick = function (event){
+
+    function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+     console.log(bgColor);
+  
+    document.body.style.background = bgColor;
+    }
+
+random_bg_color();
+}
+
+    const helpers = {
+
+        removeClass: (cssClass) => {
+          if (document.body.classList.contains(cssClass)) {
+            document.body.classList.remove(cssClass);
+          }
+        },
+
+    generateCoordinates: () => {
+        const x = Math.floor(Math.random() * window.innerWidth);
+        const y = Math.floor(Math.random() * window.innerHeight);
+        return { x, y };
+      },
+
+generateRandomColorString: (alpha = true) => {
+       // random RBG values
+        const r = Math.floor(Math.random() * 255);
+        const g = Math.floor(Math.random() * 255);
+        const b = Math.floor(Math.random() * 255);
+        // fully opaque if `false` is passed to function
+        let a = 1;
+        if (alpha) {
+          a = Math.random();
+        }
+  
+        return `rgba(${r},${g},${b},${a})`;
+      }
+
+
+    }
